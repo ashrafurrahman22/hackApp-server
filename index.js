6f76589dda2940a132a3fb46c589c66c0b3dab2c
ASHRAFUR RAHMAN <you@example.com>
2023-01-06 08:47:11 +0600

    updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { urlencoded } = require('express');
 const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -9,6 +10,8 @@ const app = express();
 // middleware
 app.use(cors())
 app.use(express.json());
+// app.use(urlencoded({ extended: true }));
+app.use(urlencoded({extended:true}));
 
 
 
